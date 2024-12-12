e264404e0de3aa7511569225931c9e091e18b7a5
Berkay526 <berkayalpar1@gmail.com>
2024-12-12 19:45:41 +0300

    improve readability of vehicles.cpp

--- vehicles.cpp
+++ vehicles.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
 #include <string>
 
+// generate a new class
 class Vehicle {
 private:
-    std::string name;
-    std::string color;
-    int age;
+    //declare variables
+    std::string m_name;
+    std::string m_color;
+    int m_age;
 
 public:
-    Vehicle(std::string name, std::string color, int age) : name(name), color(color), age(age) {}
+    // declare the constructor function
+    Vehicle(std::string m_name, std::string m_color, int m_age)
+     : m_name(m_name), 
+       m_color(m_color), 
+       m_age(m_age) {}
 
+    // make const function (func doesn't modify the variables) so that it can access to both const and non-const variables
     void display() const {
-        std::cout << "Name : " << name 
-                  << "\nColor : " << color 
-                  << "\nAge : " << age 
+        std::cout << "Name : " << m_name 
+                  << "\nColor : " << m_color 
+                  << "\nAge : " << m_age 
                   << std::endl;
     }
 };
 
 class Car : public Vehicle { 
 private:
-    std::string brand;
-    int engineHP;
+    // declare additional variables
+    std::string m_brand;
+    int m_engineHP;
 
 public:
-    Car(std::string name, std::string color, int age, std::string brand, int engineHP)
-        : Vehicle(name, color, age), brand(brand), engineHP(engineHP) {}
+    // declare the constructor function of Car class
+    Car(std::string m_name, std::string m_color, int m_age, std::string m_brand, int m_engineHP)
+        : Vehicle(m_name, m_color, m_age), m_brand(m_brand), m_engineHP(m_engineHP) {}
 
     void displayCar() const {
         display(); 
-        std::cout << "Brand : " << brand 
-                  << "\nEngine HP : " << engineHP 
+        std::cout << "Brand : " << m_brand 
+                  << "\nEngine HP : " << m_engineHP 
                   << "\n"
                   << std::endl;
     }
@@ -38,24 +47,25 @@ public:
 
 class Airplane : public Vehicle { 
 private:
-    std::string brand;
-    int engineHP;
-    double wingLengthMeters;
+    // declare additional variables
+    std::string m_brand;
+    int m_engineHP;
+    double m_wingLengthMeters;
 
 public:
-    Airplane(std::string name, std::string color, int age, std::string brand, int engineHP, double wingLengthMeters)
-        : Vehicle(name, color, age), brand(brand), engineHP(engineHP), wingLengthMeters(wingLengthMeters) {}
+    // declare the constructor function of Airplane class
+    Airplane(std::string m_name, std::string m_color, int m_age, std::string m_brand, int m_engineHP, double m_wingLengthMeters)
+        : Vehicle(m_name, m_color, m_age), m_brand(m_brand), m_engineHP(m_engineHP), m_wingLengthMeters(m_wingLengthMeters) {}
 
     void displayPlane() const {
         display(); 
-        std::cout << "Brand : " << brand 
-                  << "\nEngine HP : " << engineHP 
-                  << "\nWing Length (meters) : " << wingLengthMeters
+        std::cout << "Brand : " << m_brand 
+                  << "\nEngine HP : " << m_engineHP 
+                  << "\nWing Length (meters) : " << m_wingLengthMeters
                   << "\n"
                   << std::endl;
     }
 };
-
 
 int main() {
     Car car1("Ferrari SF90 Spider", "Red", 4, "Ferrari S.p.A.", 1000);
@@ -63,4 +73,6 @@ int main() {
     
     Airplane plane1("Airbus A330-300" , "White", 32, "Airbus", 28000, 60.3);
     plane1.displayPlane();
+
+    return 0;
 }
