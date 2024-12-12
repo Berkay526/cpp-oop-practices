1381a92c1d802fd9b25bd12eaac88d4f5a244eb7
Berkay526 <berkayalpar1@gmail.com>
2024-12-12 19:30:40 +0300

    add airplanes class to vehicles super class

--- vehicles.cpp
+++ vehicles.cpp
@@ -31,12 +31,36 @@ public:
         display(); 
         std::cout << "Brand : " << brand 
                   << "\nEngine HP : " << engineHP 
+                  << "\n"
                   << std::endl;
     }
 };
 
+class Airplane : public Vehicle { 
+private:
+    std::string brand;
+    int engineHP;
+    double wingLengthMeters;
+
+public:
+    Airplane(std::string name, std::string color, int age, std::string brand, int engineHP, double wingLengthMeters)
+        : Vehicle(name, color, age), brand(brand), engineHP(engineHP), wingLengthMeters(wingLengthMeters) {}
+
+    void displayPlane() const {
+        display(); 
+        std::cout << "Brand : " << brand 
+                  << "\nEngine HP : " << engineHP 
+                  << "\nWing Length (meters) : " << wingLengthMeters
+                  << "\n"
+                  << std::endl;
+    }
+};
+
+
 int main() {
     Car car1("Ferrari SF90 Spider", "Red", 4, "Ferrari S.p.A.", 1000);
     car1.displayCar(); 
-    return 0;
+    
+    Airplane plane1("Airbus A330-300" , "White", 32, "Airbus", 28000, 60.3);
+    plane1.displayPlane();
 }
